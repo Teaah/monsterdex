da34bb5149d4e573e52d1dc93dbe8ac5a2b458b3
Tonya <admin@tonyachung.com>
2020-02-01 20:14:12 -0500

    Updates

---
+++ precache-manifest.f01a164cfd7ca4797715fdfc6a7eea6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c09e4e1a29893f98f7862de76d81c14",
+    "url": "/monsterdex/index.html"
+  },
+  {
+    "revision": "2a3ae12f68232182ab65",
+    "url": "/monsterdex/static/css/main.d2afe97a.chunk.css"
+  },
+  {
+    "revision": "30443df84d6fc50cb31f",
+    "url": "/monsterdex/static/js/2.84d26529.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsterdex/static/js/2.84d26529.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a3ae12f68232182ab65",
+    "url": "/monsterdex/static/js/main.71acba06.chunk.js"
+  },
+  {
+    "revision": "ec0e116ec032f0684a74",
+    "url": "/monsterdex/static/js/runtime-main.92a0f519.js"
+  }
+]);
